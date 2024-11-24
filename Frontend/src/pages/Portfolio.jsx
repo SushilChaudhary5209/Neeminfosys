46ddcc378bcabe5fa41c3c171aa49dc0e8b7388d
Sushil Chaudhary <sushilchaudhary0408@gmail.com>
2024-11-24 18:57:01 +0545

    3rd comment

--- Frontend/src/pages/Portfolio.jsx
+++ Frontend/src/pages/Portfolio.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { IoIosArrowRoundForward } from 'react-icons/io';
-import Lba_logo2 from '../../public/images/Lba.png';
+import Lba_logo2 from '../../public/images/logo1.png';
 import Lba_college from '../../public/images/Lba-college.png';
 import Mantra from '../../public/images/mantra.png';
 import mantra_hospital from '../../public/images/mantra-hospital.png';
