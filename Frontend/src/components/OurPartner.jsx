b3cdd2ac3d822eb17d286ec9e1ba4bafe152a55f
Sushil Chaudhary <sushilchaudhary0408@gmail.com>
2024-11-24 20:34:30 +0545

    fix: build fix

--- Frontend/src/components/OurPartner.jsx
+++ Frontend/src/components/OurPartner.jsx
@@ -1,36 +1,36 @@
 
 import React from 'react';
 
-import sudurpashchim_upvc from '../../public/images/sudurpashchim-upvc.jpg';
-import jai_siddha_baba from '../../public/images/jai-siddha-baba.png';
-import Brahma_upvc from '../../public/images/Brahma.png';
-import timber_group_logo from '../../public/images/timber_group_logo.png';
-import Planet_VT from '../../public/images/Planet VT_logo.png';
-import rcit_logo from '../../public/images/rcit_logo.png';
-import Kakalu_mart from '../../public/images/kakalu-mart.png';
-import sagarmatha_choudhary_eye_hospital_logo from '../../public/images/sagarmatha choudhary eye hospital_logo.png';
-import powerpointnepal_logo from '../../public/images/powerpointnepal_logo.png';
-import dstourstravels_logo from '../../public/images/dstours&travels_logo.png';
-import academycambridgeeducation_logo from '../../public/images/academycambridgeeducation_logo.png';
-import brmraramasala_logo from '../../public/images/brmraramasala_logo.png';
-import taximandu_logo from '../../public/images/taximandu_logo.png';
-import nepalfireandsafetysolution_logo from '../../public/images/nepalfireandsafetysolution_logo.png';
-import jpenergy_logo_20231128_134238 from '../../public/images/jpenergy_logo_20231128_134238.png';
-import bsrbiotech_logo from '../../public/images/bsrbiotech_logo.png';
-import dminpolyclinic_logo from '../../public/images/dminpolyclinic_logo.png';
-import neeldavidssalon_academy_logo from '../../public/images/neeldavidssalon&academy_logo.png';
-import sahararidesharing_logo from '../../public/images/sahararidesharing_logo.png';
-import shramonlineconsult_logo from '../../public/images/shramonlineconsult_logo.png';
-import SumnimaHerbal from '../../public/images/Sumnima Herbal.png';
-import livpure_nepal from '../../public/images/livpure-nepal.jpg';
-import longevity from '../../public/images/longevity.jpg';
-import balmiki_international from '../../public/images/balmiki-international.png';
-import little_buddha_academy from '../../public/images/little-buddha-academy.png';
-import mantre1 from '../../public/images/mantre1.jpg';
-import logo_heaven from '../../public/images/logo-heaven.png';
-import chitlang_village_resort from '../../public/images/chitlang-village-resort.png';
-import homesnepal from '../../public/images/homesnepal 1.png';
-import Hotel911 from '../../public/images/Hotel911.png';
+import sudurpashchim_upvc from '/images/sudurpashchim-upvc.jpg';
+import jai_siddha_baba from '/images/jai-siddha-baba.png';
+import Brahma_upvc from '/images/Brahma.png';
+import timber_group_logo from '/images/timber_group_logo.png';
+import Planet_VT from '/images/Planet VT_logo.png';
+import rcit_logo from '/images/rcit_logo.png';
+import Kakalu_mart from '/images/Kakalu_mart.png';
+import sagarmatha_choudhary_eye_hospital_logo from '/images/sagarmatha choudhary eye hospital_logo.png';
+import powerpointnepal_logo from '/images/powerpointnepal_logo.png';
+import dstourstravels_logo from '/images/dstours&travels_logo.png';
+import academycambridgeeducation_logo from '/images/academycambridgeeducation_logo.png';
+import brmraramasala_logo from '/images/brmraramasala_logo.png';
+import taximandu_logo from '/images/taximandu_logo.png';
+import nepalfireandsafetysolution_logo from '/images/nepalfireandsafetysolution_logo.png';
+import jpenergy_logo_20231128_134238 from '/images/jpenergy_logo_20231128_134238.png';
+import bsrbiotech_logo from '/images/bsrbiotech_logo.png';
+import dminpolyclinic_logo from '/images/dminpolyclinic_logo.png';
+import neeldavidssalon_academy_logo from '/images/neeldavidssalon&academy_logo.png';
+import sahararidesharing_logo from '/images/sahararidesharing_logo.png';
+import shramonlineconsult_logo from '/images/shramonlineconsult_logo.png';
+import SumnimaHerbal from '/images/Sumnima Herbal.png';
+import livpure_nepal from '/images/livpure-nepal.jpg';
+import longevity from '/images/longevity.jpg';
+import balmiki_international from '/images/balmiki-international.png';
+import little_buddha_academy from '/images/little-buddha-academy.png';
+import mantre1 from '/images/mantre1.jpg';
+import logo_heaven from '/images/logo-heaven.png';
+import chitlang_village_resort from '/images/chitlang-village-resort.png';
+import homesnepal from '/images/homesnepal 1.png';
+import Hotel911 from '/images/Hotel911.png';
 
 const partnerImages = [
     sudurpashchim_upvc, jai_siddha_baba, Brahma_upvc, timber_group_logo,
